c40520336f85c5d4bf3911f61860bba36567bbaa
Dustin Dortch <me@dustindortch.com>
2025-07-02 12:37:27 -0400

    Test environment variable

--- main.go
+++ main.go
@@ -1,45 +1,21 @@
 package main
 
 import (
-	"cmp"
 	"fmt"
-	"log"
-	"os"
+
+	"github.com/sethvargo/go-githubactions"
 )
 
 var Name string
 
 func init() {
-	Name = cmp.Or(
-		os.Getenv("INPUT_NAME"),
-		"World",
-	)
-}
-
-func SetOutput(k string, v string) error {
-	f, err := os.OpenFile(os.Getenv("GITHUB_OUTPUT"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
+	Name = githubactions.GetInput("name")
+	if Name == "" {
+		githubactions.Fatalf("missing input 'name'")
 	}
-	if _, err := f.Write([]byte(fmt.Sprintf("%s=%s\n", k, v))); err != nil {
-		return err
-	}
-	if err := f.Close(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func Printenv(e string) {
-	log.Println(os.Getenv(e))
 }
 
 func main() {
-	Printenv("GITHUB_OUTPUT")
 	greeting := fmt.Sprintf("Hello, %v!", Name)
-	log.Println(greeting)
-	err := SetOutput("greeting", greeting)
-	if err != nil {
-		log.Fatalf("Error setting output: %v", err)
-	}
+	githubactions.SetOutput("greeting", greeting)
 }
