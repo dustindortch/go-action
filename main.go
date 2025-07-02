1b431a34f27fcd5d57ed332023748728ae6bf00d
Dustin Dortch <me@dustindortch.com>
2025-07-02 12:08:40 -0400

    Test environment variable

--- main.go
+++ main.go
@@ -20,7 +20,12 @@ func output(k string, v string) string {
 	return fmt.Sprintf(`::set-output name=%s::%s`, k, v)
 }
 
+func Printenv(e string) {
+	fmt.Println(os.Getenv(e))
+}
+
 func main() {
+	Printenv("GITHUB_OUTPUT")
 	greeting := fmt.Sprintf("Hello, %v!", Name)
 	log.Println(greeting)
 	fmt.Println(output("greeting", greeting))
